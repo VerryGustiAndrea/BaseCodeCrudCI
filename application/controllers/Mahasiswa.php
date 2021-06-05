3efc7a88bd08d08cf2577303c6eb1573ee2b30fa
verrygustiandrea <verrygustiandrea@gmail.com>
2021-06-05 18:13:18 +0800

    get databse

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -1,11 +1,22 @@
 <?php
 
 class Mahasiswa extends CI_Controller{
+
+    //Load model agar bisa digunakan di satu controller
+      public function __construct(){
+        parent::__construct();
+        $this->load->model('Mahasiswa_model');
+
+    }
+
+
     public function index(){
+        // $this->load->database();
 
         $data['judul'] = 'Daftar Mahasiswa';
+        $data['mahasiswa'] = $this->Mahasiswa_model->getAllMahasiswa();
         $this->load->view('templates/header', $data);
-        $this->load->view('mahasiswa/index');
+        $this->load->view('mahasiswa/index', $data);
         $this->load->view('templates/footer');
     }
 }
--- application/views/mahasiswa/index.php
+++ application/views/mahasiswa/index.php
@@ -4,7 +4,9 @@
        <div class="col-md-3">
        <div class="h3">Daftar Mahasiswa</div>
             <ul class="list-group">
-                <li class="list-group-item">An item</li>
+            <?php foreach($mahasiswa as $mhs):?>
+                <li class="list-group-item">A<?= $mhs['nama_provinsi']; ?></li>
+                <?php endforeach; ?>
             </ul>
         </div>
     </div>
