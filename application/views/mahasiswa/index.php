3e6e55eab6512d11e6016b912f2f51a4039d246b
verrygustiandrea <verrygustiandrea@gmail.com>
2021-06-05 17:41:37 +0800

    basecode

---
+++ application/views/mahasiswa/index.php
@@ -0,0 +1,11 @@
+<div class="container">
+
+    <div class="row mt-3">
+       <div class="col-md-3">
+       <div class="h3">Daftar Mahasiswa</div>
+            <ul class="list-group">
+                <li class="list-group-item">An item</li>
+            </ul>
+        </div>
+    </div>
+</div>
