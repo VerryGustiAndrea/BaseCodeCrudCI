8fc9ad37409b62a3b65c4e9e4a7f1a3fde8c7845
verrygustiandrea <verrygustiandrea@gmail.com>
2021-06-05 18:24:36 +0800

    navbar route base url

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -6,6 +6,7 @@ class Mahasiswa extends CI_Controller{
       public function __construct(){
         parent::__construct();
         $this->load->model('Mahasiswa_model');
+        $this->load->helper('url');
 
     }
 
